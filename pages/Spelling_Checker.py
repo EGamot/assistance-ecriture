5f1bbb6e27bcf9a73e251db2b097a5c3ce94180e
EGamot <168175943+EGamot@users.noreply.github.com>
2024-04-26 11:47:25 +0200

    Update Spelling_Checker.py
    Use secrets

--- pages/Spelling_Checker.py
+++ pages/Spelling_Checker.py
@@ -8,8 +8,8 @@ from langchain.prompts import PromptTemplate
 from langchain_core.prompts.chat import PromptTemplate
 import time
 import streamlit as st
-from dotenv import load_dotenv
-import os
+#from dotenv import load_dotenv
+#import os
 
 # Page title
 st.sidebar.header("Spelling Checker")
@@ -39,8 +39,8 @@ def coherence_spelling_4(content:str) -> str:
         {content} 
     
     """ # noqa E501
-    load_dotenv()
-    azure_ad_token = os.getenv('TOKEN_OPENAI_GPT4')
+    #load_dotenv()
+    azure_ad_token = st.secrets.TOKEN_OPENAI_GPT4
     llm_4 = AzureChatOpenAI(
         deployment_name="poc-aar-sgdy-gpt4-turbo",
         temperature=0,
