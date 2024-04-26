8a149f59f482a5529e203a144300f11882648f89
EGamot <168175943+EGamot@users.noreply.github.com>
2024-04-26 11:46:28 +0200

    Update Keywords_Extractor.py
    use secrets

--- pages/Keywords_Extractor.py
+++ pages/Keywords_Extractor.py
@@ -8,8 +8,8 @@ from langchain.prompts import PromptTemplate
 from langchain_core.prompts.chat import PromptTemplate
 
 import streamlit as st
-from dotenv import load_dotenv
-import os
+#from dotenv import load_dotenv
+#import os
 
 # Page title
 st.sidebar.header("Keyword Extractor")
@@ -46,8 +46,8 @@ def key_4(text: str) -> dict:
     TEXT : 
         {content}
     """ # noqa E501
-    load_dotenv()
-    azure_ad_token = os.getenv('TOKEN_OPENAI_GPT4')
+    #load_dotenv()
+    azure_ad_token = st.secrets.TOKEN_OPENAI_GPT4
 
     llm_4 = AzureChatOpenAI(
         deployment_name="poc-aar-sgdy-gpt4-turbo",
